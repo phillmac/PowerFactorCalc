01a08c8752fcf0b3b429dd17983b134a3d7191c7
Phill <phill@peelvalley.com.au>
2018-10-03 20:49:37 +1000

    Save changes

--- PowerFactor.js
+++ PowerFactor.js
@@ -205,19 +205,9 @@ var pfCalc = (function(){
                 var prevX = prevLocation.x;
                 var prevY = prevLocation.y;
                 params.endLocation = new (_geom.point)(prevX + params.reactivePower, prevY + params.truePower);
-                conol.log('created new endpoint')
             }
         }
-
-        console.log({
-            'hasApparentPower': hasApparentPower,
-            'hasReactivePower': hasReactivePower,
-            'hasTruePower': hasTruePower,
-            'hasPhaseAngle': hasPhaseAngle,
-            'hasLocation': hasLocation
-        })
-
-        
+       
         var newLoad = new _load(params.apparentPower, params.truePower, params.reactivePower, params.phaseAngle, params.endLocation);
         var newLocation = newLoad.endLocation;
     
